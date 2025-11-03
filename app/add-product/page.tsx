abd0ca0e6a0d02ef239f2c4b25b60294bfacca81
binsoy69 <binslegendary@gmail.com>
2025-11-03 14:15:27 +0800

    done with add-product page

---
+++ app/add-product/page.tsx
@@ -0,0 +1,143 @@
+import Sidebar from "@/components/sidebar";
+import { createProduct } from "@/lib/actions/products";
+import { getCurrentUser } from "@/lib/auth";
+import Link from "next/link";
+
+export default async function AddProductPage() {
+  const user = await getCurrentUser();
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Sidebar currentPath="/add-product" />
+
+      <main className="ml-64 p-8">
+        {/* Header */}
+        <div className="mb-8">
+          <div className="flex items-center justify-between">
+            <div>
+              <h1 className="text-2xl font-semibold text-gray-900">
+                Add Product
+              </h1>
+              <p className="text-sm text-gray-500">
+                Add a new product to your inventory.
+              </p>
+            </div>
+          </div>
+        </div>
+        {/* End Header */}
+
+        <div className="max-w-2xl">
+          <div className="bg-white rounded-lg border border-gray-200 p-6">
+            <form className="space-y-6" action={createProduct}>
+              {/* For Product name */}
+              <div>
+                <label
+                  htmlFor="name"
+                  className="block text-sm font-medium text-gray-700 mb-2"
+                >
+                  Product Name *
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  required
+                  className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:border-transparent"
+                  placeholder="Enter Product Name"
+                />
+              </div>
+
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                {/* For Product Quantity */}
+                <div>
+                  <label
+                    htmlFor="quantity"
+                    className="block text-sm font-medium text-gray-700 mb-2"
+                  >
+                    Quantity *
+                  </label>
+                  <input
+                    type="number"
+                    id="quantity"
+                    name="quantity"
+                    min="0"
+                    required
+                    className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:border-transparent"
+                    placeholder="0"
+                  />
+                </div>
+                {/* For Product Price */}
+                <div>
+                  <label
+                    htmlFor="price"
+                    className="block text-sm font-medium text-gray-700 mb-2"
+                  >
+                    Product Price *
+                  </label>
+                  <input
+                    type="number"
+                    id="price"
+                    name="price"
+                    step="0.01"
+                    min="0"
+                    required
+                    className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:border-transparent"
+                    placeholder="0.0"
+                  />
+                </div>
+              </div>
+              {/* For Product SKU */}
+              <div>
+                <label
+                  htmlFor="sku"
+                  className="block text-sm font-medium text-gray-700 mb-2"
+                >
+                  SKU (Optional)
+                </label>
+                <input
+                  type="text"
+                  id="sku"
+                  name="sku"
+                  className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:border-transparent"
+                  placeholder="Enter SKU"
+                />
+              </div>
+              {/* For Low Stock */}
+              <div>
+                <label
+                  htmlFor="lowStockAt"
+                  className="block text-sm font-medium text-gray-700 mb-2"
+                >
+                  Low Stock At (Optional)
+                </label>
+                <input
+                  type="number"
+                  id="lowStockAt"
+                  name="lowStockAt"
+                  min="0"
+                  className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:border-transparent"
+                  placeholder="Enter Low stock threshold"
+                />
+              </div>
+
+              {/* For Buttons */}
+              <div className="flex gap-5">
+                <button
+                  type="submit"
+                  className="px-6 py-3 bg-blue-400 text-white rounded-lg hover:bg-blue-500"
+                >
+                  Add Product
+                </button>
+                <Link
+                  href="/"
+                  className="px-6 py-3 bg-gray-200 text-gray-800 rounded-lg hover:bg-gray-300"
+                >
+                  Cancel
+                </Link>
+              </div>
+            </form>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
